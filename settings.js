add09f6620865ca6fecf5ba50baf20bc069530ea
Mikeysmitch <154681410+Mikeysmitch@users.noreply.github.com>
2025-03-13 13:56:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
+  packname: 'mikey',
+  author: 'ondas',
   botName: "Knight Bot",
   botOwner: 'mikey', // Your name
-  ownerNumber: '+2348145758117', //Your number
+  ownerNumber: '+2349072178834', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
